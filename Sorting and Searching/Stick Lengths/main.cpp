f839fbb78fa7ef598d525c7dfa9c8827b9d6f43d
David Vuscan <dvuscan10@gmail.com>
2024-09-24 21:04:41 +0300

    Day 8

---
+++ Sorting and Searching/Stick Lengths/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+long long n, p[200001], res;
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> p[i];
+    sort(p, p + n);
+    for(int i = 0; i < n; i++) res += abs(p[n/2] - p[i]);
+    cout << res;
+    return 0;
+}
