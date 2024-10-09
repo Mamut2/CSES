83ad74eb2c7fa144123a1f20a2751aeedb7631b1
David Vuscan <dvuscan10@gmail.com>
2024-10-09 21:30:16 +0300

    Day 18

--- Graph Algorithms/High Score/main.cpp
+++ Graph Algorithms/High Score/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <climits>
-#include <queue>
 
 using namespace std;
 
-
 const long long NMAX = 2501, INF = 1e14;
-long long n, m, a, b, w, d[NMAX];
-bool g[NMAX][NMAX];
+long long n, m, a, b, w, d[NMAX], marked[NMAX], v[NMAX];
+vector<int> g[NMAX];
+bool ok = 1;
 
 struct Edge
 {
     long long a, b, w;
 } edges[2 * NMAX];
+
+void dfs(int k){
+    v[k] = 1;
+    if(marked[k]) ok = false;
+    for(auto u : g[k]) if(!v[u]) dfs(u);
+}
 
 int main(){
     ios::sync_with_stdio(0);
@@ -24,23 +28,25 @@ int main(){
     for(long long i = 0; i < m; i++){
         cin >> a >> b >> w;
         edges[i] = {a, b, w};
-        g[a][b] = 1;
+        g[b].push_back(a);
     }
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             a = edges[j].a, b = edges[j].b, w = edges[j].w;
+            if(d[b] == d[a] && d[b] == -INF) continue;
             if(d[b] < d[a] + w) d[b] = d[a] + w;
         }
     }
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            a = edges[j].a, b = edges[j].b, w = edges[j].w;
-            if(d[b] < d[a] + w) { 
-                d[b] = d[a] + w;
-                if(g[b][n]) {cout << -1; return 0;} 
-            }
+    for(int j = 0; j < m; j++){
+        a = edges[j].a, b = edges[j].b, w = edges[j].w;
+        if(d[b] < d[a] + w) { 
+            if(d[b] == d[a] && d[b] == -INF) continue;
+            d[b] = d[a] + w;
+            marked[b] = 1;
         }
     }
-    cout << d[n];
+    dfs(n);
+    if(ok) cout << d[n];
+    else cout << -1;
     return 0;
 }
