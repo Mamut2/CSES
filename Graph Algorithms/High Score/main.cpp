6552e316a77cb0b86245ede82f46e9fe59cbe683
Mamut <dvuscan10@gmail.com>
2024-10-09 10:46:59 +0300

    Day 18

---
+++ Graph Algorithms/High Score/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+
+const long long NMAX = 2501, INF = 1e14;
+long long n, m, a, b, w, d[NMAX];
+bool g[NMAX][NMAX];
+
+struct Edge
+{
+    long long a, b, w;
+} edges[2 * NMAX];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for(long long i = 2; i <= n; i++) d[i] = -INF;
+    d[1] = 0;
+    for(long long i = 0; i < m; i++){
+        cin >> a >> b >> w;
+        edges[i] = {a, b, w};
+        g[a][b] = 1;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            a = edges[j].a, b = edges[j].b, w = edges[j].w;
+            if(d[b] < d[a] + w) d[b] = d[a] + w;
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            a = edges[j].a, b = edges[j].b, w = edges[j].w;
+            if(d[b] < d[a] + w && g[b][n]) { cout << -1; return 0; }
+        }
+    }
+    cout << d[n];
+    return 0;
+}
