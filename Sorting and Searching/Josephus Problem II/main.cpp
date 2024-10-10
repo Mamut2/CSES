d9cce05f22431ae323c7cd2750892e4f62e65c14
David Vuscan <dvuscan10@gmail.com>
2024-10-10 19:51:38 +0300

    Day 19

--- Sorting and Searching/Josephus Problem II/main.cpp
+++ Sorting and Searching/Josephus Problem II/main.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
-#include <queue>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
-int n, cnt, k, m[2 * (int)1e5];
-queue<int> q;
+const int NMAX = 1e5;
+int n, root, k, row = 0, col = 0;
+vector<vector<int>> m;
+vector<int> v;
 
 int main(){
     cin >> n >> k;
-    for(int i = 1; i <= n; i++) q.push(i), m[i] = k % i;
-    while (!q.empty())
-    {
-        int x = q.front();
-        if(cnt == m[q.size()]) cout << x << ' ', cnt = -1, q.pop();
-        else q.pop(), q.push(x);
-        cnt++;
+    root = sqrt(n);
+    for(int i = 1; i <= n; i++){
+        if(v.size() > root) m.push_back(v), v.clear();
+        v.push_back(i);
+    }
+    if(!v.empty()) m.push_back(v), v.clear();
+    
+    for(int i = 0; i < n; i++){
+        int el = k % (n - i);
+        while(el){
+            if(m[row].size() > el + col) col += el, el = 0;
+            else{
+                el -= (m[row].size() - col);
+                col = 0;
+                row++;
+            }  
+            if(row >= m.size()) row = 0;
+        }
+        while (m[row].size() <= col)
+        {
+            row++;
+            col = 0;
+            if(row >= m.size()) row = 0;
+        }
+        cout << m[row][col] << ' ';
+        if(i != n - 1) {
+            m[row].erase(m[row].begin() + col);
+            while (m[row].size() <= col)
+            {
+                row++;
+                col = 0;
+                if(row >= m.size()) row = 0;
+            }
+        }
     }
     return 0;
 }
